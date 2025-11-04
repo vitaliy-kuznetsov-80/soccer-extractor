6f4ba5788a6603c12d34ebf9f656ae875ac2a393
VItaly Kuznetsov <122791115+KuznetsovVitaliy@users.noreply.github.com>
2025-11-04 15:51:35 +0700

    Add version

--- build.cmd
+++ build.cmd
@@ -4,3 +4,8 @@ rmdir /s /q build
 xcopy "assets" "dist\assets" /s /e /i
 mkdir "dist\config"
 copy "config\appsettings.json" "dist\config"
+
+set /p Version=<version
+tar -a -c -f release_%Version%.zip dist
+
+rmdir /s /q dist
